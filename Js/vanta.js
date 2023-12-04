d3706f473812332de2429cfb2f7bee81d595b86a
Zero Two <mifuniree@gmail.com>
2023-12-04 14:22:30 +0700

    add vanta

---
+++ Js/vanta.js
@@ -0,0 +1,18 @@
+window.addEventListener('DOMContentLoaded', () => {
+    VANTA.BIRDS({
+        el: "#vanta",
+        mouseControls: true,
+        touchControls: true,
+        gyroControls: false,
+        minHeight: 200.00,
+        minWidth: 200.00,
+        scale: 1.00,
+        scaleMobile: 1.00,
+        color2: 0xd009de,
+        birdSize: 2.00,
+        wingSpan: 40.00,
+        speedLimit: 3.20,
+        quantity: 3.00,
+        backgroundAlpha: 0.0
+    })
+})
